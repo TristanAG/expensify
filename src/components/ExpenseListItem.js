af3f9247d3f338cad371be4e96f42f4aa9c314f5
TristanAG <tgruener@nordicnaturals.com>
2018-01-16 09:38:24 -0800

    setup formatting for amount and created at

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')}
+      -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
   </div>
 );
 
-// const mapStateToProps = (state) => {
-//   return {
-//     actions: state.actions
-//   };
-// };
-
 export default ExpenseListItem;
-
-
-//so the mistake that i had here was that I didn't.. well i'm not sure yet.
-//i basically had all this, i just was not destructuring the  props
